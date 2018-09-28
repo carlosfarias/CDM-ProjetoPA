5180f980f0f04aa485a023e5a7178dbb8f0e58d0
Carlos Farias <dev.carlos.farias@gmail.com>
2018-09-28 15:42:23 -0300

    Atualização

--- js/main.js
+++ js/main.js
@@ -1,45 +1,37 @@
 function Gerar() {
-    a1 = document.calc.a1.value;
-    r = document.calc.r.value;
-    n = document.calc.n.value;
-    tipoPA = document.calc.tipo[0].checked; // PA ou PG
+        a1 = document.calc.a1.value;
+        r = document.calc.r.value;
+        n = document.calc.n.value;
+        document.calc.resultado.value = "";
+        resultado = "";
 
-    document.calc.resultado.value = "";
-    resultado = "";
+        if (isNaN(a1) || a1 == "") {
+          alert("Digite o primeiro termo da progressão");
+          document.calc.a1.focus();
+          return false;         
+        }
+        if (isNaN(r) || r == "") {
+          alert("Digite a razão da progressão");
+          document.calc.r.focus();
+          return false;         
+        }
+        if (isNaN(n) || n == "") {
+          alert("Digite a quantidade de termos da progressão");
+          document.calc.n.focus();
+          return false;         
+        }
 
-    if (isNaN(a1) || a1 == "") {
-      alert("Digite o primeiro termo da progress�o");
-      document.calc.a1.focus();
-      return false;         
-    }
-    if (isNaN(r) || r == "") {
-      alert("Digite a raz�o da progress�o");
-      document.calc.r.focus();
-      return false;         
-    }
-    if (isNaN(n) || n == "") {
-      alert("Digite a quantidade de termos da progress�o");
-      document.calc.n.focus();
-      return false;         
-    }
+        termo = new Array();
 
-    termo = new Array();
+        resultado = a1 + ", ";
 
-    resultado = a1 + ", ";
+         // se for PA     
+          for (i = 2; i <= n; i++) {
+            termo[i] = a1 * 1 + ((i - 1) * r);
+            resultado += termo[i] + ", ";
+          }             
 
-    if (tipoPA) {   // se for PA     
-      for (i = 2; i <= n; i++) {
-        termo[i] = a1 * 1 + ((i - 1) * r);
-        resultado += termo[i] + ", ";
-      }          
-    }
-    else {
-      for (i = 2; i <= n; i++) {
-        termo[i] = a1 * Math.pow(r, i - 1);
-        resultado += termo[i] + ", ";
-      }        
-    }
-    resultado = resultado.substring(0, resultado.length - 2);  // elimina a �ltima v�rgula
-    document.calc.resultado.value = resultado;
-    return false;
-  }
+        resultado = resultado.substring(0, resultado.length - 2);  // elimina a última vírgula
+        document.calc.resultado.value = resultado;
+        return false;
+      }
